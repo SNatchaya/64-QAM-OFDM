afcba8ec7c8b448f1a2c38373b550e46e6d35db2
S. Natchaya <pv.natchaya@gmail.com>
2020-11-06 00:14:34 +0700

    test again

--- test.py
+++ test.py
@@ -1,31 +1,26 @@
-import os
-import cv2
-import time
-
-# init camera
-execution_path = os.getcwd()
-camera = cv2.VideoCapture(0)
-
-while True:
-    # Init and FPS process
-    start_time = time.time()
-
-    # Grab a single frame of video
-    ret, frame = camera.read()
-
-    # calculate FPS >> FPS = 1 / time to process loop
-    fpsInfo = "FPS: " + str(1.0 / (time.time()-start_time()))
-    print(fpsInfo)
-
-    cv2.putText(frame, fpsInfo, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+# import the opencv library 
+import cv2 
+  
+  
+# define a video capture object 
+vid = cv2.VideoCapture(0) 
+  
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+  
+    # Display the resulting frame 
+    cv2.imshow('frame', frame) 
+      
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
-
-# Release handle to the webcam
-camera.release()
-cv2.destroyAllWindows()
+  
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
